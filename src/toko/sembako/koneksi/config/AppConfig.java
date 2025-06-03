9c47c40d75e0aef8ab2c655a5ce60f824fe56f5d
Hendra_Yana <hendrabaru2525@gmail.com>
2025-06-03 15:19:49 +0700

    Add files via upload

---
+++ src/toko/sembako/koneksi/config/AppConfig.java
@@ -0,0 +1,8 @@
+package toko.sembako.config;
+
+public class AppConfig {
+    public static final String APP_NAME = "Manajemen Stok Barang Toko Sembako";
+    public static final String DB_URL = "jdbc:mysql://localhost:3306/toko_sembako";
+    public static final String DB_USER = "root";
+    public static final String DB_PASSWORD = "";
+}
